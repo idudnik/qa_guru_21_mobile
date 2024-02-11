310692ec1e87588f27fbdec9f35c7ed64f22bc77
Stanislav Dudnik <136478699+idudnik@users.noreply.github.com>
2024-02-11 11:47:18 +0300

    добавлен .енв, добавлено открытие страницы

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -4,7 +4,6 @@ from selene import browser, have
 
 
 def test_search_appium():
-
     with step('open page'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
@@ -14,12 +13,13 @@ def test_search_appium():
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
 
+
 def test_search_python():
     with step('open page'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Python')
-
     with step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Python'))
+        results.first.click()
